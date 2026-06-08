f4c6f0c28f9a265c9c76b02dd3d3f9c45f21bbde
kernal-pony <67898649+kernal-pony@users.noreply.github.com>
2026-06-08 15:40:28 +0530

    Add SQL query for July 2022 active user retention

---
+++ session_5/Active User Retention.sql
@@ -0,0 +1,14 @@
+
+SELECT
+    7 AS month,
+    COUNT(DISTINCT curr_month.user_id) AS monthly_active_users
+FROM user_actions curr_month
+WHERE EXTRACT(MONTH FROM curr_month.event_date) = 7
+  AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
+  AND EXISTS (
+        SELECT 1
+        FROM user_actions last_month
+        WHERE last_month.user_id = curr_month.user_id
+          AND EXTRACT(MONTH FROM last_month.event_date) = 6
+          AND EXTRACT(YEAR FROM last_month.event_date) = 2022
+  );
